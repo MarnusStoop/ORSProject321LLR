686f4d0b7d5ac6be2323a5ee4648672810a7d283
LordSkaven <marnusstoop@hotmail.co.za>
2017-08-23 11:49:21 +0200

    Some changes to model and generating canonical form

--- ORSProject/Program.cs
+++ ORSProject/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ORSProjectModels;
 
 namespace ORSProject
 {
@@ -9,6 +10,44 @@ namespace ORSProject
     {
         static void Main(string[] args)
         {
+            Model m = GenerateSampleModel();
+            Console.WriteLine(m.GenerateDisplayableCanonical());
+            Console.ReadLine();
         }
+
+        static Model GenerateSampleModel()
+        {
+            OptimizationType type = OptimizationType.Max;
+            string[] decVars = { "x1", "x2", "x3" };
+            double[] objFunc = { 4, 5, -3 };
+            List<Constraint> constraints = GenerateSampleConstraints();
+            List<SignRestriction> restrictions = GenerateSampleSignRestrictions();
+            return new Model(type, decVars, objFunc, constraints, restrictions);
+        }
+
+        static List<Constraint> GenerateSampleConstraints()
+        {
+            Constraint con1 = new Constraint(new double[] { 2, 1, 4 }, Sign.LessEqual, 5);
+            Constraint con2 = new Constraint(new double[] { -1, 2, 2 }, Sign.GreaterEqual, -6);
+            Constraint con3 = new Constraint(new double[] { 3, 0, -1 }, Sign.Equal, 2);
+            List<Constraint> constraints = new List<Constraint>();
+            constraints.Add(con1);
+            constraints.Add(con2);
+            constraints.Add(con3);
+            return constraints;
+        }
+
+        static List<SignRestriction> GenerateSampleSignRestrictions()
+        {
+            List<SignRestriction> restrictions = new List<SignRestriction>();
+            SignRestriction res1 = new SignRestriction("x1", Positivity.Positive, RestrictionType.Decimal);
+            SignRestriction res2 = new SignRestriction("x2", Positivity.Negative, RestrictionType.Decimal);
+            SignRestriction res3 = new SignRestriction("x3", Positivity.Unrestricted, RestrictionType.Decimal);
+            restrictions.Add(res1);
+            restrictions.Add(res2);
+            restrictions.Add(res3);
+            return restrictions;
+        }
+
     }
 }
--- ORSProjectModels/ORSProjectModels/Models/Model.cs
+++ ORSProjectModels/ORSProjectModels/Models/Model.cs
@@ -12,6 +12,9 @@ namespace ORSProjectModels
         private double[] objectiveFunction;
         private List<Constraint> constraints;
         private List<SignRestriction> signRestrictions;
+        private double[][] canonicalForm;
+
+        private int numberOfConstraints = 0;
 
         public OptimizationType OptimizationType
         {
@@ -43,6 +46,12 @@ namespace ORSProjectModels
             set { signRestrictions = value; }
         }
 
+        public double[][] CanonicalForm
+        {
+            get { return canonicalForm; }
+            set { canonicalForm = value; }
+        }
+
         public Model()
         {
 
@@ -55,6 +64,59 @@ namespace ORSProjectModels
             this.ObjectiveFunction = objectiveFunction;
             this.Constraints = constraints;
             this.SignRestrictions = signRestrictions;
+            GenerateCanonicalForm();
+        }
+
+        private void GenerateCanonicalForm()
+        {
+            int numberOfRows = 0;
+            //Amount of decision variables plus 1 for rhs
+            //int numberOfColumns = decisionVariables.Length + 1;
+            foreach (var item in constraints)
+            {
+                numberOfRows++;
+                //numberOfColumns++;
+                if (item.Sign == Sign.Equal)
+                {
+                    numberOfRows++;
+                    //numberOfColumns++;
+                }
+            }
+            canonicalForm = new double[numberOfRows][];
+            //Assign objective
+            canonicalForm[0] = ConvertObjective();
+            canonicalForm[1] = ConvertObjective();
+            canonicalForm[2] = ConvertObjective();
+            canonicalForm[3] = ConvertObjective();
+        }
+
+        private double[] ConvertObjective()
+        {
+            double[] converted = objectiveFunction;
+            for (int i = 0; i < converted.Length; i++)
+            {
+                converted[i] *= -1;
+            }
+            return converted;
+        }
+
+        private void GenerateInitialTable()
+        {
+
+        }
+
+        public string GenerateDisplayableCanonical()
+        {
+            string displayable = "";
+            foreach (var item in canonicalForm)
+            {
+                foreach (var val in item)
+                {
+                    displayable += val + " ";
+                }
+                displayable += "\n";
+            }
+            return displayable;
         }
 
         public override bool Equals(object obj)
